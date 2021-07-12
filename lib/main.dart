ffd8112b8ace966f3bbe13e42ff477ddc6c52941
ahmedhossain29 <hossainahmad76@gmail.com>
2021-07-12 21:18:27 +0600

    fast

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/statelesswidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ lib/statelesswidget.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home Page'),
+        ),
+      ),
+    );
+  }
+}
