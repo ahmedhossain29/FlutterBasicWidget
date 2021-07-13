7589faf638585a6016b58dbb9a216ac1ceec6ebe
ahmedhossain29 <hossainahmad76@gmail.com>
2021-07-13 15:34:57 +0600

    List View & SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/appBar_textWidget.dart';
 import 'package:flutter_application_1/column_Row.dart';
 import 'package:flutter_application_1/containerWidget.dart';
+import 'package:flutter_application_1/listview_scrollview.dart';
 import 'package:flutter_application_1/safearea.dart';
 import 'package:flutter_application_1/statelesswidget.dart';
 
 void main() {
-  runApp(column_row());
+  runApp(listView());
 }
