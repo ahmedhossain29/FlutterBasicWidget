d6043dcac8aba019a0b655cedcb46318d7493726
ahmedhossain29 <hossainahmad76@gmail.com>
2021-07-13 18:08:44 +0600

    Media Query for Responsive Design

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter_application_1/appBar_textWidget.dart';
 import 'package:flutter_application_1/column_Row.dart';
 import 'package:flutter_application_1/containerWidget.dart';
 import 'package:flutter_application_1/listview_scrollview.dart';
+import 'package:flutter_application_1/mediaQueryWidget.dart';
 import 'package:flutter_application_1/safearea.dart';
 import 'package:flutter_application_1/statelesswidget.dart';
 
 void main() {
-  runApp(listView());
+  runApp(MediaQueryWidget());
 }
