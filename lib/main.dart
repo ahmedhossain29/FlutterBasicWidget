8c6c0217cebdd863e88951063fb3ca1cdc0f21a4
ahmedhossain29 <hossainahmad76@gmail.com>
2021-07-13 18:28:47 +0600

    ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/appBar_textWidget.dart';
 import 'package:flutter_application_1/column_Row.dart';
 import 'package:flutter_application_1/containerWidget.dart';
+import 'package:flutter_application_1/listTile.dart';
 import 'package:flutter_application_1/listview_scrollview.dart';
 import 'package:flutter_application_1/mediaQueryWidget.dart';
 import 'package:flutter_application_1/safearea.dart';
 import 'package:flutter_application_1/statelesswidget.dart';
 
 void main() {
-  runApp(MediaQueryWidget());
+  runApp(ListTileWidget());
 }
