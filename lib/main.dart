2dd0b880ae68204fe3287cb809cbc36c45f72950
ahmedhossain29 <hossainahmad76@gmail.com>
2021-07-14 16:37:30 +0600

    Card And Grid View

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/ImageWidget.dart';
 import 'package:flutter_application_1/appBar_textWidget.dart';
+import 'package:flutter_application_1/card_gridView.dart';
 import 'package:flutter_application_1/column_Row.dart';
 import 'package:flutter_application_1/containerWidget.dart';
 import 'package:flutter_application_1/listTile.dart';
@@ -10,5 +11,5 @@ import 'package:flutter_application_1/safearea.dart';
 import 'package:flutter_application_1/statelesswidget.dart';
 
 void main() {
-  runApp(ImageWidget());
+  runApp(Card_gridView());
 }
